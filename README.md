d87ad7d6e6d59398e8479c6b020d8905ff251bba
jackwang <wangxichao001@gmail.com>
2017-07-14 00:54:42 +0800

    add branch files

---
+++ README.md
@@ -0,0 +1,216 @@
+# vue2-tree-origin
+
+# README
+这是功能定制版本， 异步加载数据
+
+loading 提示
+
+注意适用方式
+generateKey and loadingChild 方法
+
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+
+-----
+### 贡献者名单
+ - lily7129
+### develop logs
+- 2017-5-2: 支持下拉树, add combotree
+- 2017-5-26: 添加父节点半选状态框 
+- 2017-6-08: 修复火狐复选事件的bug 
+- 2017-6-21: 修复动态渲染
+### QQ交流群:255965810
+### How to install the plugin
+```
+ npm install vue2-tree or cnpm install vue2-tree (国内)
+```
+### How to run demo
+```
+npm install
+npm run dev 
+```
+### 在线Demo
+ [点击进入线上效果](https://halower.github.io/vue2-tree/)
+### 效果图
+ ![效果图](http://files.cnblogs.com/files/rohelm/jdfw.gif)
+### 示例
+```html
+<template>
+    <div id="app" style="width:300px; margin: auto auto;">
+        <tree
+                ref='tree'
+                :treeData="treeData"
+                :options="options"
+                @node-click="itemClick"
+
+        />
+    </div>
+</template>
+<script>
+    import Vue from 'vue';
+    import axios from 'axios';
+    import Tree from './components/tree/tree.vue';
+    let that = null
+    export default {
+        name: 'app',
+        data () {
+            that = this;
+            return {
+                options: {
+                    showCheckbox: false,
+                    halfCheckedStatus: false,//控制父框是否需要半钩状态
+
+                    lazy: true,
+                    load: this.loadingChild,
+
+                    showSearch: false,
+                    search: {
+                        useInitial: true,
+                        useEnglish: false,
+                        customFilter: null
+                    }
+                },
+                treeData: []
+            }
+        },
+        mounted: function () {
+            this.loadTreeData();
+        },
+        methods: {
+            /**
+             * generate key 0-1-2-3
+             * this is very important function for now module
+             * @param treeData
+             * @param parentKey
+             * @returns {Array}
+             */
+            generateKey (treeData = [], parentKey) {
+                treeData = treeData.map((item, i) => {
+                    item.key = parentKey + '-' + i.toString();
+                    return item;
+                })
+                return treeData;
+            },
+            loadTreeData: function () {
+                let treeData = [
+                    {
+                        id: 1,
+                        label: '一级节点',
+                        open: false,
+                        checked: false,
+                        nodeSelectNotAll: false,//新增参数，表示父框可以半钩状态
+                        parentId: null,
+                        visible: true,
+                        searched: false
+                    },
+                    {
+                        id: 2,
+                        label: '一级节点',
+                        open: false,
+                        checked: false,
+                        nodeSelectNotAll: false,//新增参数，表示父框可以半钩状态
+                        parentId: null,
+                        visible: true,
+                        searched: false
+                    }
+                ];
+
+                this.treeData = this.generateKey(treeData, 0);
+            },
+            async loadingChild (node, index) {
+                try {
+                    let tem;
+                    let postions = node.key.split('-');
+                    // load json file you need another server
+                    let data = await axios.get(' http://172.16.0.104:8082/child.json');
+
+                    for (let [index, item] of postions.entries()) {
+                        switch (index) {
+                            case 0:
+                                break;
+                            case 1:
+                                tem = this.treeData[item];
+                                break;
+                            default:
+                                tem = tem.children[item];
+                        }
+                    }
+                    // set Children
+                    Vue.set(tem, 'children', this.generateKey(data.data, node.key) );
+
+                    Promise.resolve(data);
+                } catch (e) {
+                    Promise.reject(e);
+                }
+            },
+            itemClick (node) {
+                console.log(node.key);
+            }
+        },
+        components: {
+            Tree
+        }
+    }
+</script>
+
+```
+### 属性
+| 参数      | 说明    | 类型      | 可选值 | 默认值  |
+|---------- |-------- |---------- |---------- |---------- |
+|options     | 配置项 | Object | — | — |
+| treeData | 指定节点对象数组 | Array[Object] | — | — |
+
+```
+     options: {
+        showCheckbox: true,  //是否支持多选,
+        halfCheckedStatus: true,//控制父框是否需要半钩状态,
+        
+        lazy: true,     // 是否是异步加载数据
+        load: this.loadingChild, // 异步加载数据方法
+        showSearch: false, // 是否显示搜索
+        
+        search: {
+          useInitial: true, //是否支持拼音首字母搜索
+          useEnglish: false, //是否是英文搜索
+          customFilter: null // 自定义节点过滤函数
+        }
+
+    /* 节点元素 */
+    {
+      id: 1, //节点标志
+      label: '一级节点', //节点名称
+      open: true, // 是否打开节点
+      checked: false, //是否被选中
+      parentId: null, //父级节点Id
+      visible: true, //是否可见
+      searched: false, //是否是搜索值,
+      nodeSelectNotAll: false,//表示父框可以半钩状态
+      children: [] //子节点
+    }
+```
+### 方法
+| 方法名      | 说明    | 参数      |
+|---------- |-------- |---------- |
+| getSelectedNodeIds  | 若节点可被选择则返回目前被选中的节点Id所组成的数组 | 被选中的节点Id所组成的数组 |
+| getSelectedNodes  | 若节点可被选择则返回目前被选中的节点组成的数组 | 被选中的节点所组成的数组 |
+
+### 事件
+| 事件名称      | 说明    | 回调参数      |
+|---------- |-------- |---------- |
+| node-click  | 节点被点击时的回调 | 共1个参数，节点组件本身。 |
+
---
+++ src/components/tree/tree-node.vue
@@ -0,0 +1,365 @@
+<template>
+    <ul>
+        <li v-for='(item, index) of nodeData'
+            v-show="item.visible"
+            :key="item.key"
+        >
+            <i v-if=" item.children && item.children.length > 0  ||  options.hasOwnProperty('lazy') && !item.hasOwnProperty('loaded') "
+               @click.stop='handleNodeExpand(item, index)'
+               class="icon iconfont icon-color"
+               :class="[ !options.hasOwnProperty('lazy') || item.open? 'icon-jian-fangkuang':'icon-jia-fangkuang', 'icon']"
+            >
+            </i>
+            </span>
+
+            <div class="inputCheck"
+                 :class="{notAllNodes:item.nodeSelectNotAll}"
+                 :style="{width:inputWidth+'px', height:inputWidth+'px'}"
+                 v-show="options.showCheckbox"
+                 @click="walkCheckBox(item)"
+            >
+                <input type="checkbox" class="check"
+                       v-if="options.showCheckbox && options.halfCheckedStatus  &&  !item.nodeSelectNotAll"
+                       v-model='item.checked'
+                       @change="handlecheckedChange(item)"
+                />
+            </div>
+            <span
+                class="icon-color halo-tree-icon_loading halo-tree-iconEle"
+                v-show="item.loading"
+            >
+
+            </span>
+            <span
+                    @click="handleNode(item)"
+                    class="label"
+                    :class="{'node-selected':(item.checked && !options.showCheckbox) || item.searched }"
+            >
+                {{item.label}}
+            </span>
+            <tree-node v-if="item.children && item.children.length > 0"
+                       :options="options"
+                       @handlecheckedChange="handlecheckedChange"
+                       v-show='item.open'
+                       :tree-data="item.children"
+
+            >
+            </tree-node>
+        </li>
+    </ul>
+</template>
+<script>
+    import Vue from 'vue'
+    export default {
+        name: 'treeNode',
+        props: {
+            treeData: [Array],
+            options: [Object]
+        },
+        data () {
+            return {
+                nodeData: []
+            }
+        },
+        created () {
+            const parent = this.$parent
+            if (parent.isTree) {
+                this.tree = parent
+            } else {
+                this.tree = parent.tree
+            }
+
+            const tree = this.tree
+            if (!tree) {
+                console.warn('找不到树节点')
+            }
+            this.nodeData = (this.treeData || []).slice(0)
+        },
+        computed: {
+            inputWidth: function () {
+                if (this.checkFirfox()) {
+                    return 14
+                }
+                return 13
+            }
+        },
+        watch: {
+            treeData: function (data) {
+                this.nodeData = (data || []).slice(0)
+            }
+        },
+        methods: {
+            checkFirfox(){
+                let u = navigator.userAgent
+                if (u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1) {
+                    return true
+                }
+                return false
+            },
+            walkCheckBox(node){
+                if (node.nodeSelectNotAll) {
+                    node.checked = !node.checked
+                    this.handlecheckedChange(node)
+                }
+            },
+            handleNodeExpand (node, index) {
+                if (node.open) {
+                    Vue.set(node, 'open', false)
+                    return false
+                }
+                if (node.hasOwnProperty('children')) {
+                    Vue.set(node, 'open', true)
+                    return
+                }
+
+                if (!node.open  && this.options.lazy && this.options.load) {
+
+                    try {
+                        Vue.set(node, 'loading', true)
+
+                        this.options.load(node, index).then((d) => {
+                            Vue.set(node, 'open', true)
+                            Vue.set(node, 'loaded', true)
+                            Vue.set(node, 'loading', false)
+                        })
+
+
+                    } catch (e) {
+                        console.log('Get Child Erroe')
+                    }
+                }
+            },
+            handlecheckedChange (node) {
+                this.$emit('handlecheckedChange', node)
+            },
+            handleNode (node) {
+                debugger
+                if (this.tree.store.last) {
+                    if (this.tree.store.last.key === node.key) {
+                        this.tree.store.last.checked = !this.tree.store.last.checked
+                        Vue.set(node, 'checked', this.tree.store.last.checked)
+                    } else {
+                        Vue.set(this.tree.store.last, 'checked', false)
+                        Vue.set(node, 'checked', true)
+                        this.tree.store.last = node
+                    }
+                } else {
+                    this.tree.store.last = node
+                    Vue.set(node, 'checked', true)
+
+                }
+                this.tree.$emit('node-click', node)
+            }
+        }
+    }
+</script>
+<style scoped>
+
+    @font-face {
+        font-family: "iconfont";
+        src: url('./../../assets/iconfont/iconfont.eot?t=1499924440773'); /* IE9*/
+        src: url('./../../assets/iconfont/iconfont.eot?t=1499924440773#iefix') format('embedded-opentype'), /* IE6-IE8 */ url('./../../assets/iconfont/iconfont.woff?t=1499924440773') format('woff'), /* chrome, firefox */ url('./../../assets/iconfont/iconfont.ttf?t=1499924440773') format('truetype'), /* chrome, firefox, opera, Safari, Android, iOS 4.2+*/ url('./../../assets/iconfont/iconfont.svg?t=1499924440773#iconfont') format('svg'); /* iOS 4.1- */
+    }
+
+    .iconfont {
+        font-family: "iconfont" !important;
+        font-size: 16px;
+        font-style: normal;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    .icon-jia-fangkuang:before {
+        content: "\e8de";
+    }
+
+    .icon-jian-fangkuang:before {
+        content: "\e8e0";
+    }
+
+    .icon-loading:before {
+        content: "\e647";
+    }
+
+    .icon-color {
+        color: #108ee9;
+    }
+
+    /**  todo   **/
+    .halo-tree li span.halo-tree-iconEle {
+        margin: 0;
+        width: 24px;
+        height: 24px;
+        line-height: 24px;
+        display: inline-block;
+        vertical-align: middle;
+        border: 0 none;
+        cursor: pointer;
+        outline: none;
+        text-align: center;
+    }
+
+    .halo-tree li span.halo-tree-icon_loading::after {
+        display: inline-block;
+        font-family: 'iconfont';
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        content: "\e647";
+        -webkit-animation: loadingCircle 1s infinite linear;
+        animation: loadingCircle 1s infinite linear;
+
+    }
+
+    @keyframes loadingCircle {
+        0% {
+            transform-origin: 50% 50%;
+            transform: rotate(0deg);
+        }
+        100% {
+            transform-origin: 50% 50%;
+            transform: rotate(360deg);
+        }
+    }
+
+    li:hover {
+        cursor: pointer;
+    }
+
+    .halo-tree {
+        font-size: 14px;
+        min-height: 20px;
+        -webkit-border-radius: 4px;
+        -moz-border-radius: 4px;
+        border-radius: 4px;
+    }
+
+    .node-selected {
+        background-color: #ddd
+    }
+
+    .halo-tree li {
+        margin: 0;
+        padding: 5px 5px 5px 0;
+        position: relative;
+        list-style: none;
+        user-select: none;
+    }
+
+    .halo-tree li > span,
+    .halo-tree li > i,
+    .halo-tree li > a {
+        line-height: 20px;
+        vertical-align: middle;
+    }
+
+    .halo-tree ul ul li:hover {
+        background: rgba(0, 0, 0, .035)
+    }
+
+    .halo-tree li:after,
+    .halo-tree li:before {
+        content: '';
+        left: -8px;
+        position: absolute;
+        right: auto;
+        border-width: 1px
+    }
+
+    .halo-tree li:before {
+        border-left: 1px dashed #999;
+        bottom: 50px;
+        height: 100%;
+        top: -8px;
+        width: 1px;
+
+    }
+
+    .halo-tree li:after {
+        border-top: 1px dashed #999;
+        height: 20px;
+        top: 17px;
+        width: 12px
+    }
+
+    .halo-tree li span {
+        display: inline-block;
+        padding: 3px 3px;
+        text-decoration: none;
+        border-radius: 3px;
+        margin-left: 4px;
+    }
+    .halo-tree li span.label {
+        margin-left: 0px;
+    }
+
+    .halo-tree li:last-child::before {
+        height: 26px
+    }
+
+    .halo-tree > ul {
+        padding-left: 0
+    }
+
+    .halo-tree ul ul {
+        padding-left: 19px;
+        padding-top: 10px;
+    }
+
+    .halo-tree li.leaf {
+        padding-left: 19px;
+    }
+
+    .halo-tree li.leaf:after {
+        content: '';
+        left: -8px;
+        position: absolute;
+        right: auto;
+        border-width: 1px;
+        border-top: 1px dashed #999;
+        height: 20px;
+        top: 17px;
+        width: 24px;
+    }
+
+    .check {
+        display: inline-block;
+        position: relative;
+        top: 4px;
+    }
+
+    .halo-tree .icon {
+        margin-left: 4px;
+    }
+
+    .search {
+        width: 14px;
+        height: 14px;
+        background-image: url("../../assets/search.png");
+    }
+
+    /*.check.notAllNodes{
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      -ms-appearance: none;
+      width: 13px;
+    }*/
+    .inputCheck {
+        display: inline-block;
+        position: relative;
+    }
+
+    .inputCheck.notAllNodes:before {
+        content: "";
+        display: inline-block;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        z-index: 10;
+        top: 50%;
+        left: 50%;
+        transform: translate3d(-30%, -5%, 0);
+        /*background-image: url("/../../assets/half.png");*/
+        background-image: url("../../assets/half.jpg");
+    }
+</style>
