b97fc3f0722777f6b3365f5761fb266ef6dbe1fc
jackwang <wangxichao001@gmail.com>
2017-07-13 15:20:21 +0800

    add postcss autoprefix

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -3,10 +3,15 @@ var config = require('../config')
 var isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  loaders: utils.cssLoaders({
-    sourceMap: isProduction
-      ? config.build.productionSourceMap
-      : config.dev.cssSourceMap,
-    extract: isProduction
-  })
+    loaders: utils.cssLoaders({
+        sourceMap: isProduction
+            ? config.build.productionSourceMap
+            : config.dev.cssSourceMap,
+        extract: isProduction
+    }),
+    postcss: [
+        require('autoprefixer')({
+            browsers: ['last 3 versions']
+        })
+    ]
 }
