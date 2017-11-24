38337f22d08558081b054b162e66ec72bba58b2c
halower <121625933@qq.com>
2017-11-24 19:46:02 +0800

    fixed all checked

--- src/components/vtree/render.js
+++ src/components/vtree/render.js
@@ -6,7 +6,6 @@ export default {
     tpl: Function
   },
   render (h, ctx) {
-    debugger
     return ctx.props.tpl ? ctx.props.tpl(ctx.props.node, ctx) : <span domPropsInnerHTML={ctx.props.node.title} onClick={() => ctx.parent.expandNode(ctx.props.node)}></span>
   }
 }
