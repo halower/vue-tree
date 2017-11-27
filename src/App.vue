0d2b8507935eefd633f5e4a3181fb6df67c5f640
田云 <tianyun@nationsky.com>
2017-11-27 14:36:53 +0800

    Half-checked box style is updated.

--- src/App.vue
+++ src/App.vue
@@ -21,4 +21,22 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.halo-tree-searching {
+
+}
+.halo-tree-search-box {
+  height: 18px;
+  line-height: 18px;
+  outline: none;
+  border: 1px solid #888888;
+  border-radius: 3px;
+}
+.halo-tree-search-box:focus {
+  border: 1px solid rgb(16, 142, 233);
+  -webkit-box-shadow: 0 2px 2px rgba(16, 142, 233, .2);
+  box-shadow: 0 2px 2px rgba(16, 142, 233,.2);
+  -webkit-transition: border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;
+  -o-transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
+  transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
+}
 </style>
