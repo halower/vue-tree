93fe321bfe504658fbe74058afcea5db0a7f019a
谢海龙 <xiehailong@163.com>
2017-11-29 17:33:17 +0800

    update to mulitple function

--- src/pages/demo/HelloWorld.vue
+++ src/pages/demo/HelloWorld.vue
@@ -10,7 +10,7 @@
        <option value='en'>Engilsh</option>
     </select>
     <input type="text" v-model="searchexpression" :class="{'halo-tree-searching': searchexpression, 'halo-tree-search-box': true}"/>
-    <tree ref='tree' :searchexpression='searchexpression' :data='treeData' :multiple='true' :tpl='tpl' :halfcheck='true'></tree>
+    <tree ref='tree' :searchexpression='searchexpression' :data='treeData' :multiple='false' :tpl='tpl' :halfcheck='true'></tree>
  </div>
 </template>
 
@@ -74,13 +74,17 @@ export default {
     },
     tpl (node, ctx) {
       let titleClass = node.selected ? 'node-title node-selected' : 'node-title'
-      if (node.searched) {
-        titleClass += ' node-searched'
-      }
+      if (node.searched) titleClass += ' node-searched'
       return <span>
         <button style='color:blue; background-color:pink' onClick={() => this.$refs.tree.addNode(node, {title: '同步节点'})}>+</button>
       <span class={titleClass} domPropsInnerHTML={node.title} onClick={() => ctx.parent.nodeSelected(ctx.props.node)}></span>
       <button style='color:green; background-color:pink' onClick={() => this.asyncLoad(node)}>异步加载</button>
+      <button style='color:red; background-color:pink' onClick={() => this.$refs.tree.delNode(node.parent, node)}>删除</button>
+        <button style='color:green; background-color:pink' onClick={() => this.asyncLoad(node)}>异步加载</button>
+      <button style='color:red; background-color:pink' onClick={() => this.$refs.tree.delNode(node.parent, node)}>删除</button>
+        <button style='color:green; background-color:pink' onClick={() => this.asyncLoad(node)}>异步加载</button>
+      <button style='color:red; background-color:pink' onClick={() => this.$refs.tree.delNode(node.parent, node)}>删除</button>
+        <button style='color:green; background-color:pink' onClick={() => this.asyncLoad(node)}>异步加载</button>
       <button style='color:red; background-color:pink' onClick={() => this.$refs.tree.delNode(node.parent, node)}>删除</button>
       </span>
     },
