3ebeb7f14df1b289e5d36944dfe9f56ab28be06c
谢海龙 <xiehailong@163.com>
2017-12-04 17:12:53 +0800

    update code

---
+++ src/lib/mixins.js
@@ -0,0 +1,35 @@
+function guid () {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+    let r = Math.random() * 16 | 0
+    let v = c === 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
+}
+
+function setDragNode (guid, node) {
+  window['treeDrag'] = {}
+  window['treeDrag'][guid] = node
+}
+
+function getDragNode (guid) {
+  return window['treeDrag'][guid]
+}
+
+function hasInGenerations (root, node) {
+  if (root.hasOwnProperty('children') && root.children) {
+    for (let rn of root.children) {
+      if (rn === node) return true
+      if (rn.children) return hasInGenerations(rn, node)
+    }
+    return false
+  }
+}
+
+export default {
+  methods: {
+    guid,
+    setDragNode,
+    getDragNode,
+    hasInGenerations
+  }
+}
